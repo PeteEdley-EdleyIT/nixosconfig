bf9010504d48b50f5f83cad0b774bbda2e2e7bfc
Peter Edley <peter@edleyit.com>
2024-04-17 18:45:21 +0100

    Added a few more programs and gnome extensions

--- configuration.nix
+++ configuration.nix
@@ -18,17 +18,13 @@
 
   nixpkgs.config.allowUnfree = true; 
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "SurfacePro"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/London";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
@@ -82,11 +78,14 @@
     gnomeExtensions.dash-to-dock
     gnomeExtensions.caffeine
     gnomeExtensions.extension-list
+    gnomeExtensions.appindicator
     vlc
     vscode
     gh
     pavucontrol
     fish
+    onlyoffice-bin_7_5
+    nextcloud-client
   ];
 
   programs.bash = {
